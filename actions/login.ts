144e0d07477ecf066eaa13c0016c54153fea03b5
Usama <usama-365@protonmail.com>
2024-02-03 00:37:01 +0500

    fix typo

--- actions/login.ts
+++ actions/login.ts
@@ -5,7 +5,7 @@ import { LoginSchema, LoginSchemaType } from "@/schemas";
 export async function login(values: LoginSchemaType) {
   const validatedFields = LoginSchema.safeParse(values);
 
-  if (!validatedFields) return { error: "Invalid fields" };
+  if (!validatedFields.success) return { error: "Invalid fields" };
 
   return { success: "Email sent!" };
 }
--- actions/register.ts
+++ actions/register.ts
@@ -5,7 +5,7 @@ import { RegisterSchema, RegisterSchemaType } from "@/schemas";
 export async function register(values: RegisterSchemaType) {
   const validatedFields = RegisterSchema.safeParse(values);
 
-  if (!validatedFields) return { error: "Invalid fields" };
+  if (!validatedFields.success) return { error: "Invalid fields" };
 
   return { success: "Email sent!" };
 }
